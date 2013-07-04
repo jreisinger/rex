473f92624ada05e066484409c346a8c93d6b15dd
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-07-04 18:03:22 +0200

    Moved VMs' credentials to Rexfile

--- Rexfile
+++ Rexfile
@@ -1,5 +1,7 @@
 # enable new Features
 use Rex -feature => 0.40;
+# load helper subroutines
+use amazon qw(file_exists);
 
 # set your username
 set user => "<user>";
@@ -13,7 +15,16 @@ set -passauth;
 # put your server in this group
 set group => "servers" => "server1", "server2";
 
+####################
+# Credentials to Amazon (EC2) VMs
+####################
+user "root";
+my $pub_key  = File::Spec->catfile( $ENV{PWD}, ".pub-key.key" );
+my $priv_key = File::Spec->catfile( $ENV{PWD}, ".priv-key.pem" );
+return 0 unless file_exists($pub_key);
+return 0 unless file_exists($priv_key);
+public_key $pub_key;
+private_key $priv_key;
 
 # now load every module via ,,require''
 require amazon;
-
--- lib/amazon.pm
+++ lib/amazon.pm
@@ -10,21 +10,13 @@ use Data::Dumper;
 # Amazon credentials
 ####################
 
-# read content of pass.yml into $content
+# read content of $pass_file into $content
 my $pass_file = File::Spec->catfile( $ENV{PWD}, ".pass.yml" );
 return 0 unless file_exists($pass_file);
 my $content = eval { local ( @ARGV, $/ ) = ($pass_file); <>; };
 
 # load yaml into perl hashRef
 my $config = Load($content);
-
-user "root";
-my $pub_key  = File::Spec->catfile( $ENV{PWD}, ".pub-key.key" );
-my $priv_key = File::Spec->catfile( $ENV{PWD}, ".priv-key.pem" );
-return 0 unless file_exists($pub_key);
-return 0 unless file_exists($priv_key);
-public_key $pub_key;
-private_key $priv_key;
 
 my $access_key        = $config->{auth}->{access_key};
 my $secret_access_key = $config->{auth}->{password};
@@ -33,8 +25,14 @@ cloud_auth "$access_key", "$secret_access_key";
 cloud_region "ec2.eu-west-1.amazonaws.com";
 
 ####################
-sub file_exists {
+# Helper subroutines
 ####################
+
+# export those used in Rexfile
+use Exporter qw(import);
+our @EXPORT_OK = qw(file_exists);
+
+sub file_exists {
     my $file = shift;
     unless ( -e $file ) {
         print "'$file' does not exist\n";
@@ -43,9 +41,7 @@ sub file_exists {
     return 1;
 }
 
-####################
 sub vm_name2id {
-####################
     my $name = shift;
 
     for my $instance ( cloud_instance_list() ) {
